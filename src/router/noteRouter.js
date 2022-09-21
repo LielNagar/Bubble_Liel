12cbad5aec8fccd31c5eda86fa99a9a218a6753e
LielNagar <104072448+LielNagar@users.noreply.github.com>
2022-09-21 14:58:39 +0300

    Get all notes feature is working!

--- src/router/noteRouter.js
+++ src/router/noteRouter.js
@@ -2,6 +2,14 @@ const express=require('express')
 const knex = require('../../knex/knex')
 const router= new express.Router()
 
-//TODO
+router.get('/allnotes', async(req,res)=>{
+    try{
+        const notes= await knex.select().from('notes')
+        if(!notes) res.status(404).send()
+        res.send(notes)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
 
 module.exports= router
