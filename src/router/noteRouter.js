f67359403df8c6085f8c8344e1e93403247e9693
LielNagar <104072448+LielNagar@users.noreply.github.com>
2022-09-21 15:15:53 +0300

    Feature is working on feature branch

--- src/router/noteRouter.js
+++ src/router/noteRouter.js
@@ -2,6 +2,16 @@ const express=require('express')
 const knex = require('../../knex/knex')
 const router= new express.Router()
 
-//TODO
+router.patch('/note/:id', async(req,res)=>{
+    try{
+        knex('notes').where('id',req.params.id).update({
+            is_deleted:true
+        }).then(()=>{
+            res.status(200).send()
+        })
+    }catch(e){
+        res.status(400).send()
+    }
+})
 
 module.exports= router
