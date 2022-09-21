1e73dff7c7f294202aa665d6cb2b8e505a70903e
LielNagar <104072448+LielNagar@users.noreply.github.com>
2022-09-21 15:14:41 +0300

    for split branch

--- src/router/noteRouter.js
+++ src/router/noteRouter.js
@@ -2,16 +2,6 @@ const express=require('express')
 const knex = require('../../knex/knex')
 const router= new express.Router()
 
-router.patch('/note/:id', async(req,res)=>{
-    try{
-        knex('notes').where('id',req.params.id).update({
-            is_deleted:true
-        }).then(()=>{
-            res.status(200).send()
-        })
-    }catch(e){
-        res.status(400).send()
-    }
-})
+//TODO
 
 module.exports= router
